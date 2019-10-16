242da00f6b29a55c43d865dfccb34a46fd6b7312
Vlad <vlad.nek@flatbook.co>
2019-10-16 03:01:15 -0400

    added routines and tasks

--- VIA/screens/index.js
+++ VIA/screens/index.js
@@ -1,3 +1,5 @@
 export { ProjectsScreen } from './projects/projects'
 export { HabitsScreen } from './habits/habits'
+export { RoutinesScreen } from './routines/routines'
+export { TasksScreen } from './habits/habits'
 
