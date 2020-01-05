5af35aba25ef00c99a5fb8fd978d4f43c92eaf81
Vlad <vlad.nek@flatbook.co>
2020-01-05 00:17:29 -0500

    before adding sliding tab view

---
+++ VIA/modals/notificationTimes/notificationTimesModal.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import { Text, View, TouchableOpacity, Modal, TouchableHighlight, TextInput, FlatList } from 'react-native'; // Version can be specified in package.json
+import DateTimePicker from '@react-native-community/datetimepicker';
+import Moment from 'moment'
+
+const styles = require('./styles');
+
+var date = new Date().getDate(); //Current Date
+var month = new Date().getMonth(); //Current Month
+var year = new Date().getFullYear(); //Current Year
+var hours = new Date().getHours(); //Current Hours
+var min = new Date().getMinutes(); //Current Minutes
+var sec = new Date().getSeconds(); //Current Seconds
+
+
+const todaysDate = year + '-' + month + '-' + date;
+const dateInDate = new Date(year, month, date);
+
+
+export class NotificationTimesModal extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            itemDate: this.props.itemDate ? new Date(this.props.itemDate) : dateInDate
+        };
+    }
+
+    setDate = (event, date) => {
+        console.war
+        this.props.setDate(date.toString())
+        this.setState({
+            itemDate: date
+        });
+    }
+
+    render() {
+       
+        const { itemDate } = this.state
+        console.warn(itemDate)
+        return (
+            <Modal
+                animationType={this.props.animationType}
+                transparent={true}
+                visible={this.props.visible}
+                onRequestClose={this.props.onRequestClose}>
+                <View style={styles.outerView}>
+                    {this.props.children}
+                    <View style={styles.datePickerView}> 
+                    <DateTimePicker
+                        value={itemDate}
+                        mode='date'
+                        is24Hour={true}
+                        minimumDate={dateInDate}
+                        display="spinner"
+                        onChange={this.setDate}
+                    /></View>
+                    <View style={styles.bottomButtonsContainer}>
+                    <TouchableOpacity style={styles.bottomButtonLeft} 
+                    onPress={() => { 
+                        this.setDate(null ,itemDate)
+                        this.props.closeModal()}}>
+                        <Text style={styles.bottomButtonText}>Select</Text>
+                    </TouchableOpacity>
+                    {/* <TouchableOpacity style={styles.bottomButtonCenter} 
+                    onPress={() => {
+                        this.setDate(itemDate)
+                        this.props.closeModal}}>
+                        <Text style={styles.bottomButtonText}>Today's date</Text>
+                    </TouchableOpacity> */}
+                     <TouchableOpacity style={styles.bottomButtonRight} 
+                    onPress={
+                        this.props.closeModal}>
+                        <Text style={styles.bottomButtonText}>Close</Text>
+                    </TouchableOpacity>
+                   </View>
+                   {/* <View style={styles.bottomButtonsContainer}> */}
+                  
+                    {/* </View> */}
+                </View>
+            </Modal>
+        );
+    }
+}
---
+++ VIA/modals/notificationTimes/styles.js
@@ -0,0 +1,161 @@
+/* eslint-disable no-unused-vars*/
+import React from 'react';
+/* eslint-enable no-unused-vars*/
+import { StyleSheet, PixelRatio, Dimensions, Platform } from 'react-native';
+
+const buttonBackgroundColor = '#3a506b'
+const titleFontSize = 24
+const bodyFontSize = 20
+const buttonFontSize = 20
+const leftMargin = 5
+const fontFamily = Platform.OS == "ios" ? "Roboto-Medium" : "Roboto-Medium"
+
+const backgroundColor = "#ffffff"
+const blueColor = "#068ae8"
+const textColor = "#2d3142"
+const finishedBackgroundColor = "#a8ffe0"
+const grayColor = "#ededed"
+const placeholderColor = "#ABABAB"
+const homeColorButton = "#48A2F8"
+
+const { width, height } = Dimensions.get('window');
+
+
+module.exports = StyleSheet.create({
+
+    /* #region Outer Structure */
+    outerView: {
+        flex: 1,
+        // opacity:1,
+        flexDirection: 'column',
+        backgroundColor: 'rgba(26, 26, 26, 0.9)',
+        justifyContent: 'center',
+        alignContent: 'center'
+        // alignItems: "center",
+
+    },
+    outerViewDone: {
+        flex: 2,
+        flexDirection: 'column',
+        backgroundColor: finishedBackgroundColor,
+    },
+    /* #endregion */
+
+    datePickerView: {
+        // flex: 2,
+        // alignItems: 'center'
+
+    },
+
+    /* #region  Top Navigation */
+    topNav: {
+        marginBottom: 5,
+        flexDirection: "row",
+        justifyContent: 'space-between',
+    },
+    trashButton: {
+        marginRight: "8%",
+        justifyContent: "center",
+
+    },
+    topNavBackButton: {
+        marginLeft: "8%",
+    },
+    /* #endregion */
+
+    /* #region  Bottom Buttons */
+    bottomButtonsContainer: {
+        paddingTop: 18,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        marginBottom: 10,
+    },
+
+     /* #region Create Bottom Buttons Section */
+     bottomButtonsContainer: {
+        paddingTop: 18,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        marginBottom: 10,
+    },
+    bottomButtonLeftDisabled: {
+        marginLeft: 50,
+        paddingLeft: 35,
+        paddingRight: 35,
+        paddingTop: 10,
+        paddingBottom: 10,
+        borderRadius: 20,
+        borderWidth: 2,
+        borderColor: placeholderColor,
+        backgroundColor: placeholderColor,
+        shadowColor: "#000000",
+        shadowOpacity: 0.8,
+        shadowRadius: 2,
+        shadowOffset: {
+            height: 1,
+            width: 0
+        }
+    },
+    bottomButtonLeft: {
+        marginLeft: 50,
+        paddingLeft: 35,
+        paddingRight: 35,
+        paddingTop: 10,
+        paddingBottom: 10,
+        borderRadius: 20,
+        borderWidth: 2,
+        borderColor: blueColor,
+        backgroundColor: blueColor,
+        shadowColor: "#000000",
+        shadowOpacity: 0.8,
+        shadowRadius: 2,
+        shadowOffset: {
+            height: 1,
+            width: 0
+        }
+    },
+    bottomButtonCenter: {
+        marginLeft: 50,
+        paddingLeft: 35,
+        paddingRight: 35,
+        paddingTop: 10,
+        paddingBottom: 10,
+        borderRadius: 20,
+        borderWidth: 2,
+        borderColor: blueColor,
+        backgroundColor: blueColor,
+        shadowColor: "#000000",
+        shadowOpacity: 0.8,
+        shadowRadius: 2,
+        shadowOffset: {
+            height: 1,
+            width: 0
+        }
+    },
+    bottomButtonRight: {
+        marginLeft: 50,
+        paddingLeft: 35,
+        paddingRight: 35,
+        paddingTop: 10,
+        paddingBottom: 10,
+        borderRadius: 20,
+        borderWidth: 2,
+        borderColor: placeholderColor,
+        backgroundColor: backgroundColor,
+        marginRight: 50,
+        shadowColor: "#000000",
+        shadowOpacity: 0.8,
+        shadowRadius: 2,
+        shadowOffset: {
+            height: 1,
+            width: 0
+        }
+    },
+    bottomButtonText: {
+        fontSize: 18,
+        textAlign: 'center',
+        fontFamily: fontFamily,
+    },
+    /* #endregion */
+
+})
