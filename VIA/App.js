57d89eea7ecadced521095667ed907e6635cb8e4
Vlad <vlad.nek@flatbook.co>
2020-01-29 21:17:43 -0500

    Cleaned up code

--- VIA/App.js
+++ VIA/App.js
@@ -7,42 +7,6 @@ import { CreateHabit } from './modals'
 import SIcon from 'react-native-vector-icons/dist/SimpleLineIcons';
 import { TabButton } from '../VIA/components/tabButton'
 import { HomeButton } from '../VIA/components/homeButton'
-var uuid = require('react-native-uuid');
-
-// const defaultNavigationOptions = ({ navigation }) => ({
-//   // initialRouteName: 'Home',
-//   tabBarIcon: ({ focused, horizontal, tintColor }) => {
-//     const { routeName } = navigation.state;
-//     let IconComponent = SIcon;
-//     let iconName;
-//     if (routeName === 'Habits') {
-//       iconName = focused
-//         ? 'refresh'
-//         : 'refresh';
-//       // Sometimes we want to add badges to some icons.
-//       // You can check the implementation below.
-//       // IconComponent = HomeIconWithBadge;
-//     } else if (routeName === 'Routines') {
-//       // iconName = focused ? 'ios-list-box' : 'ios-list';
-//       iconName = focused ? 'reload' : 'reload';
-//     } else if (routeName === 'Projects') {
-//       // iconName = focused ? 'ios-list-box' : 'ios-list';
-//       iconName = focused ? 'layers' : 'layers';
-//     } else if (routeName === 'Tasks') {
-//       // iconName = focused ? 'ios-list-box' : 'ios-list';
-//       iconName = focused ? 'list' : 'list';
-//     }
-//     else if (routeName === 'Home') {
-//       // iconName = focused ? 'ios-list-box' : 'ios-list';
-//       // iconName = focused ? 'reload' : 'reload';
-//     }
-
-//     // You can return any component that you like here!
-//     return <IconComponent name={iconName} size={25} color={tintColor} />;
-//   },
-// });
-
-
 
 const TabNavigator = createBottomTabNavigator({
   Habits: {
@@ -84,7 +48,7 @@ const TabNavigator = createBottomTabNavigator({
       title: "",
       tabBarIcon:
         <HomeButton
-          nav={navigation} ></HomeButton> // Plus button component
+          nav={navigation} ></HomeButton>
     })
   },
   Projects: {
@@ -120,45 +84,9 @@ const TabNavigator = createBottomTabNavigator({
       ),})
   },
 },
-  // {defaultNavigationOptions},
   {
     initialRouteName: 'Home',
   },
-  // {
-  //   defaultNavigationOptions: ({ navigation }) => (
-  //     {
-  //     tabBarIcon: ({ focused, horizontal, tintColor }) => {
-  //       const { routeName } = navigation.state;
-  //       let IconComponent = Ionicons;
-  //       let iconName;
-  //       if (routeName === 'Habits') {
-  //         iconName = focused
-  //           ? 'refresh'
-  //           : 'refresh';
-  //       } else if (routeName === 'Routines') {
-  //         // iconName = focused ? 'ios-list-box' : 'ios-list';
-  //         iconName = focused ? 'reload' : 'reload';}
-  //         else if (routeName === 'Home') {
-  //           // iconName = focused ? 'ios-list-box' : 'ios-list';
-  //           // iconName = focused ? 'reload' : 'reload';
-  //         }
-
-  //         // You can return any component that you like here!
-  //         return <IconComponent name={iconName} size={25} color={tintColor} />;
-  //     },
-  //   },{initialRouteName: 'Home'})},
-  // {defaultNavigationOptions},
-  // {
-  //   navigationOptions: ({ navigation }) => ({
-  //     tabBarIcon: ({ focused }) => {
-
-  //     }
-  //   }),
-  // {tabBarOptions :{
-  //   activeTintColor: '#f0f',
-  // }},
-  //   initialRouteName: 'Home'
-  // },
 );
 
 Database.init();
