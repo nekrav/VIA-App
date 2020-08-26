3bb00f49e2fcf5a3ad89f04c507d9d148ba8510d
Vlad N <vlad@Vlads-Mac.local>
2020-08-26 10:30:42 -0700

    Cleaned up code

--- VIA/modals/random/random_create.js
+++ VIA/modals/random/random_create.js
@@ -65,7 +65,6 @@ const emptyTimes = [
     },
 ]
 
-
 export class CreateRandom extends React.Component {
     constructor(props) {
         super(props);
@@ -88,64 +87,63 @@ export class CreateRandom extends React.Component {
         controller.loadAll(this, Projects.TABLE_NAME);
     }
 
-/* #region  Top Bar Region */
-renderTopBar() {
-    return <TopBar
-        color={colorsProvider.randomMainColor}
-        fromCreate={true}
-        nameOfItem={this.state.name}
-        hasDueDate={true}
-        dueDate={this.state.dueDate}
-        importance={this.state.importance}
-        parent={null}
-        parentName={null}
-        allParents={null}
-        setParent={(id, name) => {
-            this.props.project(id, name);
-            this.setState({ projName: name, proj: id });
-        }}
-        removeParent={() => {
-            this.props.project(null, null)
-            this.setState({ projName: null, proj: null });
-        }}
-        closeModal={this.props.closeModal}
-        editName={item => {
-            this.setState({ name: item });
-            this.props.name(item);
-        }}
-        hasImportance={true}
-        hasParent={false}
-        setImportanceNN={() => {
-            Keyboard.dismiss()
-            this.props.setImportanceNN(1)
-        }}
-        setImportanceNU={() => {
-            Keyboard.dismiss()
-            this.props.setImportanceNU(2)
-        }}
-        setImportanceIN={() => {
-            Keyboard.dismiss()
-            this.props.setImportanceIN(3)
-        }}
-        setImportanceIU={() => {
-            Keyboard.dismiss()
-            this.props.setImportanceIU(4)
-        }}
-        selectParent={() => {
-            Keyboard.dismiss();
-            this.setProjectSelectionModalVisibility(true);
-        }}
-        selectDueDate={date => {
-            this.props.due_date(date);
-            this.setState({ dueDate: date });
-        }}
-    />
-}
-/* #endregion */
+    /* #region  Top Bar Region */
+    renderTopBar() {
+        return <TopBar
+            color={colorsProvider.randomMainColor}
+            fromCreate={true}
+            nameOfItem={this.state.name}
+            hasDueDate={true}
+            dueDate={this.state.dueDate}
+            importance={this.state.importance}
+            parent={null}
+            parentName={null}
+            allParents={null}
+            setParent={(id, name) => {
+                this.props.project(id, name);
+                this.setState({ projName: name, proj: id });
+            }}
+            removeParent={() => {
+                this.props.project(null, null)
+                this.setState({ projName: null, proj: null });
+            }}
+            closeModal={this.props.closeModal}
+            editName={item => {
+                this.setState({ name: item });
+                this.props.name(item);
+            }}
+            hasImportance={true}
+            hasParent={false}
+            setImportanceNN={() => {
+                Keyboard.dismiss()
+                this.props.setImportanceNN(1)
+            }}
+            setImportanceNU={() => {
+                Keyboard.dismiss()
+                this.props.setImportanceNU(2)
+            }}
+            setImportanceIN={() => {
+                Keyboard.dismiss()
+                this.props.setImportanceIN(3)
+            }}
+            setImportanceIU={() => {
+                Keyboard.dismiss()
+                this.props.setImportanceIU(4)
+            }}
+            selectParent={() => {
+                Keyboard.dismiss();
+                this.setProjectSelectionModalVisibility(true);
+            }}
+            selectDueDate={date => {
+                this.props.due_date(date);
+                this.setState({ dueDate: date });
+            }}
+        />
+    }
+    /* #endregion */
 
-
-     /* #region  Notification Times Region */
-     renderNotificationTimes() {
+    /* #region  Notification Times Region */
+    renderNotificationTimes() {
         return (<NotificationTimes
             color={colorsProvider.randomMainColor}
             notificationTimes={this.state.notificationTimes}
@@ -160,7 +158,7 @@ renderTopBar() {
     }
     /* #endregion */
 
-    /* #region  Only For Today Modal */
+    /* #region Only For Today */
     renderOnlyForToday() {
         return (<CheckBox
             center
@@ -184,11 +182,8 @@ renderTopBar() {
 
     /* #endregion */
 
-    
-
-     /* #region  Notes Region */
-
-     renderNotesSection() {
+    /* #region  Notes Region */
+    renderNotesSection() {
         return <Notes
             color={colorsProvider.randomMainColor}
             notes={this.state.notes}
@@ -198,78 +193,76 @@ renderTopBar() {
     }
     /* #endregion */
 
-   /* #region  Bottom Buttons Section */
-   renderBottomButtons() {
-    return (<View style={{
-        // paddingTop: 18,
-        flexDirection: "row",
-        justifyContent: "space-between",
-        marginBottom: 50,
-    }}>
-        <TouchableOpacity
-            style={{
-                flex: 1,
-                alignItems: 'center',
-                justifyContent: 'center',
-                marginLeft: 10,
-                marginRight: 10,
-                marginTop: 10,
-                marginBottom: 10,
-                borderRadius: 20,
-                backgroundColor: colorsProvider.closeButtonColor
-            }}
-            onPress={this.props.closeModal}>
-            <Text style={{
-                fontSize: 18,
-                textAlign: 'center',
-                fontFamily: colorsProvider.font,
-                color: colorsProvider.whiteColor,
-                margin: 10,
-            }}>Close</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-            disabled={this.state.name != '' ? false : true}
-            style={
-                this.state.name != ''
-                    ? {
-                        flex: 2,
-                        alignItems: 'center',
-                        justifyContent: 'center',
-                        marginLeft: 10,
-                        marginRight: 10,
-                        marginTop: 10,
-                        marginBottom: 10,
-                        borderRadius: 20,
-                        borderWidth: 2,
-                        borderColor: colorsProvider.setButtonColor,
-                        backgroundColor: colorsProvider.completeButtonColor
-                    }
-                    : {
-                        flex: 2,
-                        alignItems: 'center',
-                        justifyContent: 'center',
-                        marginLeft: 10,
-                        marginRight: 10,
-                        marginTop: 10,
-                        marginBottom: 10,
-                        borderWidth: 2,
-                        borderRadius: 20,
-                        borderColor: colorsProvider.completeButtonColor,
-                    }
-            }
-            onPress={() => {
-                notifier.scheduleAllNotifications();
-                this.props.notification_time(this.state.notificationTimes);
+    /* #region  Bottom Buttons Section */
+    renderBottomButtons() {
+        return (<View style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            marginBottom: 50,
+        }}>
+            <TouchableOpacity
+                style={{
+                    flex: 1,
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    marginLeft: 10,
+                    marginRight: 10,
+                    marginTop: 10,
+                    marginBottom: 10,
+                    borderRadius: 20,
+                    backgroundColor: colorsProvider.closeButtonColor
+                }}
+                onPress={this.props.closeModal}>
+                <Text style={{
+                    fontSize: 18,
+                    textAlign: 'center',
+                    fontFamily: colorsProvider.font,
+                    color: colorsProvider.whiteColor,
+                    margin: 10,
+                }}>Close</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+                disabled={this.state.name != '' ? false : true}
+                style={
+                    this.state.name != ''
+                        ? {
+                            flex: 2,
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            marginLeft: 10,
+                            marginRight: 10,
+                            marginTop: 10,
+                            marginBottom: 10,
+                            borderRadius: 20,
+                            borderWidth: 2,
+                            borderColor: colorsProvider.setButtonColor,
+                            backgroundColor: colorsProvider.completeButtonColor
+                        }
+                        : {
+                            flex: 2,
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            marginLeft: 10,
+                            marginRight: 10,
+                            marginTop: 10,
+                            marginBottom: 10,
+                            borderWidth: 2,
+                            borderRadius: 20,
+                            borderColor: colorsProvider.completeButtonColor,
+                        }
+                }
+                onPress={() => {
+                    notifier.scheduleAllNotifications();
+                    this.props.notification_time(this.state.notificationTimes);
 
-               
+
                     this.props.save()
-            }}>
-            <Text style={this.state.name != '' ? styles.bottomButtonTextDisabled : styles.bottomButtonText}> Save</Text>
-        </TouchableOpacity>
-    </View>)
-}
-/* #endregion */
-
+                }}>
+                <Text style={this.state.name != '' ? styles.bottomButtonTextDisabled : styles.bottomButtonText}> Save</Text>
+            </TouchableOpacity>
+        </View>)
+    }
+    /* #endregion */
 
     render() {
         return (
